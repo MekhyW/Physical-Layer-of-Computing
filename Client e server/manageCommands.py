b2123e9f9a0d2f106a3c5a788f720fa7c5aba253
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-08-25 14:06:12 -0300

    Update manageCommands.py

--- Client e server/manageCommands.py
+++ Client e server/manageCommands.py
@@ -2,7 +2,7 @@ import random
 
 def generateCommands(numberOfCommands, bytesize = 8):
     commands = ["00FA0000", "0000FA00", "FA0000", "00FA00", "0000FA", "00FA", "FA00", "00", "FA"]
-    commandList = random.choices(commands, k=numberOfCommands-len(commands)) + commands
+    commandList = random.choices(commands, k=numberOfCommands)
     random.shuffle(commandList)
     commandString = ""
     for value in range(numberOfCommands):
