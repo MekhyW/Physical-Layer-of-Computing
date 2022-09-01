eda59e3a4137c3b362b5aa39ab70d607a65ca50f
GiuseppeAvila <49525480+GiuseppeAvila@users.noreply.github.com>
2022-09-01 14:35:29 -0300

    Copia de arquivos do projeto 2 para o 3

---
+++ Projeto_3_Datagrama/manageCommands.py
@@ -0,0 +1,14 @@
+import random
+
+def generateCommands(numberOfCommands, bytesize = 8):
+    commands = ["00FA0000", "0000FA00", "FA0000", "00FA00", "0000FA", "00FA", "FA00", "00", "FA"]
+    commandList = random.choices(commands, k=numberOfCommands)
+    random.shuffle(commandList)
+    commandString = ""
+    for value in range(numberOfCommands):
+        command = commandList[value]
+        while len(command) < bytesize:
+            command = "E" + command
+        commandString += command
+    commandBytes = commandString.encode()
+    return commandBytes
---
+++ Projeto_3_Datagrama/server.py
@@ -0,0 +1,51 @@
+from enlace import *
+import time
+import numpy as np
+
+serialName = "COM7"
+
+def main():
+    try:
+        com1 = enlace(serialName)
+        com1.enable()
+
+        #Byte de sacrifício
+        print("Esperando 1 byte de sacrifício")
+        rxBuffer, nRx = com1.getData(1)
+        com1.rx.clearBuffer()
+        time.sleep(.1)
+
+        print("Comunicação aberta com sucesso")
+
+        print("Iniciando recepção de dados")
+        rxLen = com1.rx.getBufferLen()
+        while rxLen == 0:
+            rxLen = com1.rx.getBufferLen()
+            time.sleep(1)
+        rxBuffer, nRx = com1.getData(rxLen)
+
+        #Conta numero de comandos recebidos
+        bytesize = 8
+        txBuffer = len(rxBuffer.decode()) / bytesize
+        print("Quantidade de Comandos: {}".format(txBuffer))
+
+        #Enviando dados para o client
+        print("Iniciando transmissão de dados")
+        
+        com1.sendData(np.asarray(int(txBuffer)))
+            
+        # Encerra comunicação
+        print("-------------------------")
+        print("Comunicação encerrada")
+        print("-------------------------")
+        com1.disable()
+        
+    except Exception as erro:
+        print("ops! :-\\")
+        print(erro)
+        com1.disable()
+        
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
