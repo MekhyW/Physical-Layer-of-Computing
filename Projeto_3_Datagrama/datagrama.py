3f207330e43aa983124d98abe8dcd690629eda45
GiuseppeAvila <49525480+GiuseppeAvila@users.noreply.github.com>
2022-09-01 15:39:01 -0300

    Update datagrama.py

--- Projeto_3_Datagrama/datagrama.py
+++ Projeto_3_Datagrama/datagrama.py
@@ -1,12 +1,33 @@
+import re
+
+
 class Head:
     messageType: str
     senderId: str
     receiverId: str
-    totalPayloads: int
-    currentPayloadIndex: int
-    payloadSize: int
+    totalPayloads: int = 0
+    currentPayloadIndex: int = 0
+    payloadSize: int = 0
+
+    def __init__(self, messageType, senderId, receiverId):
+        self.messageType = messageType
+        self.senderId = senderId
+        self.receiverId = receiverId
+
+    def payloadData(self, totalPayloads, currentPayloadIndex, payload):
+        if self.messageType == 'DD':
+            self.totalPayloads = totalPayloads
+            self.currentPayloadIndex = currentPayloadIndex
+            self.payloadSize = len(payload)
+        else:
+            raise Exception("Error: Message type does not support non-0 payload")
+
 
 class Datagrama:
     head: Head
     payload: str = ''
     endOfPackage: str = 'FEEDBACC'
+
+    def __init__(self, head, payload):
+        self.head = head
+        self.payload = payload
