6e92e9a026bdcb0804b8b555785776f99d95523d
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-10-20 10:15:48 -0300

    Update Server.ino

--- Projeto_6_Serializacao_UART/Server/Server.ino
+++ Projeto_6_Serializacao_UART/Server/Server.ino
@@ -5,15 +5,32 @@ bool readPin() {
   return digitalRead(pin);
 }
 
+void receiveMessage() {
+  byte character = 0;
+  bool receivedBit;
+  bool parity = true;
+  for (int i=0; i<=7; i++) {
+    receivedBit = digitalRead(pin);
+    character = character + (receivedBit << i);
+    if (receivedBit == HIGH) {
+      parity = !parity;
+    }
+  }
+  if (digitalRead(pin) != parity) {
+    Serial.println("Erro: Paridade incorreta");
+  } else {
+    Serial.println(character, HEX);
+  }
+}
+
 void setup() {
   Serial.begin(baud);
   pinMode(pin, INPUT);
+  Serial.println("Aguardando sinal LOW");
+  while (readPin()) {
+  }
+  receiveMessage();
 }
 
 void loop() {
-  Serial.println(readPin());
-  //Serial.println("Aguardando sinal LOW");
-  //while (readPin()) {
-  //}
-  //Serial.println("Recebendo mensagem");
 }
